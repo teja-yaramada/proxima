00ec1110f5330306b57be1d0fb1ef4c7f6d257e6
Teja Yaramada <teja.yaramada@gmail.com>
2024-04-26 18:09:48 -0700

    Additional control in startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# Set the auto-start flag (1 to enable, 0 to disable)
+AUTO_START=1
+
+# If auto-start is disabled, exit the script
+if [ "$AUTO_START" -ne 1 ]; then
+    logger "Proxima Service: Auto-start is disabled."
+    exit 0  # exit gracefully
+fi
+
 # Set the current working directory to /home/pi2/payload/proxima/
 cd /home/pi2/payload/proxima/ || {
-    # directory change failed
+    # Directory change failed
     logger "Proxima Service: Failed to change working directory to /home/pi2/payload/proxima/"
     exit 1  # error
 }
@@ -10,5 +19,5 @@ cd /home/pi2/payload/proxima/ || {
 logger "Proxima Service: Starting sensor data collection..."
 
 # Start capture scripts 
-#python3 capture.py 1 &
-
+python3 picamera_capture.py
+# python3 capture.py 1 &
